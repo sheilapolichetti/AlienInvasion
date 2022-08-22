76e892044b389118658551b3269e12fbabb71cc7
sheila.polichetti <sheila.polichetti@hotmail.com>
2022-08-22 15:58:35 +0200

    Added functionality to move ship to the right

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,10 +28,22 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
             def _check_events(self):
                 """Respond to keypresses and mouse events."""
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        sys.exit()
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_RIGHT:
+                            self.ship.moving_right = True
+                    elif event.type == pygame.KEYUP:
+                        if event.key == pygame.K_RIGHT:
+                            self.ship.moving_right = False
+                            # Move the ship to the right
+                            self.ship.rect.x += 1
 
             def _update_screen(self):
                 """Updated images on the screen, and flip to the new screen"""
